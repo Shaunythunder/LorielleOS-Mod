eff228ddbb19afc90b9e7dc07edce31d31194bf8
Shaunythunder <shaunythunder@gmail.com>
2025-07-22 13:28:56 -0400

    6: Updated Installer for testing. Discovered that my original plan to run the installer from open OS while it was on floppy doesn't work because the drive itself is read only.  The new strategy is that I will have to have the user install open OS to be able to get the download and then the download will have to wipe the drive add freshly install the OS.  Will need to add a feature to wipe the files,  will need a backup function to backup all non OS directories to  a floppy.  Will need to add a manual on how backing up works. Will need to add a manual on how installing works.  Will need to add a reinstall feature and then will need to add a function that allows us to pull contents from a disc drive and copy them onto the main Drive. Then the installer should be able to wrap this all into a single command with prompts noting that it is a destructive install and asking for the address of the backup disk.  The GitHub repository is now public so someone might actually use this so let's code responsibly.

--- installer.lua
+++ installer.lua
@@ -1,3 +1,34 @@
 require ("internet")
 
-internet.request(
+local content = ""
+
+print("Downloading test.lua from GitHub...")
+local response = internet.request("https://raw.githubusercontent.com/Shaunythunder/LorielleOS-Mod/main/test.lua")
+print("Downloading...")
+for chunk in response do
+    content = content .. chunk
+end
+
+if #content == 0 then
+    print("Download failed. :(")
+    return
+end
+
+local file = io.open("test.lua", "w")
+file:write(content)
+file:close()
+print("File downloaded successfully!")
+
+print("Running test.lua...")
+os.execute("lua test.lua")
+print("Test completed. Cleaning up...")
+os.execute("rm -f test.lua")
+
+local file = io.open("test.lua", "r")
+if not file then
+    print("Clean up completed successfully!")
+    return
+end
+file:close()
+print("Clean up failed. :(")
+
