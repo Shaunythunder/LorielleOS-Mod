37951bb6bd1d3c06dd4aee67151dff63029f17b6
Shaunythunder <shaunythunder@gmail.com>
2025-07-25 21:14:04 -0400

    Minor update to installer.

--- disk_imager.lua
+++ disk_imager.lua
@@ -65,7 +65,7 @@ local function checkCleanWipe(path, exclusions)
     return true
 end
 
-print("Welcome to the LorielleOS Installer/Disk Imager v1.16 Alpha!")
+print("Welcome to the LorielleOS Installer/Disk Imager v1.17 Alpha!")
 print("*************************************")
 os.sleep(short_delay)
 print("Intended for use with OpenComputers.")
